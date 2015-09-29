d43c4e49110c1cd31e65e047eee9c571c6179d5b
sonicblis <slashdrew@gmail.com>
2015-09-28 21:36:39 -0400

    List of users working

---
+++ app/Users/userProvider.js
@@ -0,0 +1,22 @@
+app.service("userProvider", ['$firebaseObject', function($firebaseObject){
+    var _this = this;
+    var ref = new Firebase("https://stax.firebaseio.com/users");
+    var user = {
+        name: '',
+        id: '',
+        icon: '',
+        authenticated: false
+    };
+    this.users = $firebaseObject(ref);
+    this.getUser = function(){
+        return user;
+    };
+    this.setUserInfo = function(name, id, icon, authenticated){
+        user.name = name;
+        user.id = id;
+        user.icon = icon;
+        user.authenticated = authenticated;
+        _this.users[id] = user;
+        _this.users.$save();
+    };
+}]);

18dc0c4b90e0195537efc5d5c1f6e0b89d7a6d10
sonicblis <slashdrew@gmail.com>
2015-10-08 19:09:29 -0400

    trying to implement a slide panel for re-use

---
+++ app/global/directives/slidePanel/slidePanel.js
@@ -0,0 +1,13 @@
+app.directive("slidePanel", [function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'app/global/directives/slidePanel/slidePanel.html',
+        controller: ['$scope', function ($scope) {
+
+        }],
+        link: function ($scope, $el, $attr) {
+
+        }
+    }
+}]);
